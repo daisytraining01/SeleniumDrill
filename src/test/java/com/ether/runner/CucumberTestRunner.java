d867aee0eae87527b63d6c1fb6ee64908a30c747
Your Name <suganzv.dct@gmail.com>
2020-06-02 19:01:58 +0530

    Final Commit

--- src/test/java/com/ether/runner/CucumberTestRunner.java
+++ src/test/java/com/ether/runner/CucumberTestRunner.java
@@ -10,6 +10,7 @@ import org.testng.annotations.BeforeClass;
 import com.cucumber.listener.ExtentCucumberFormatter;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
+
 @CucumberOptions(
 		plugin = {"json:target/positive/cucumber.json", "pretty", "html:target/positive/cucumber.html","com.cucumber.listener.ExtentCucumberFormatter"},
         features = {"src/test/java/Feature_Files/"},
